7a2dc02711bef852d969e9e8c2bbb40d4511da37
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-03-13 15:01:46 +0500

    Домашние задания

---
+++ Task_54/Program.cs
@@ -0,0 +1,71 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine()!);
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+WriteLine($"Отсортированный массив: ");
+OrderArrayLines(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)// функция для заполнения 2-мерного массива рандомными элементами
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);// обращаемся к каждому элементу массива и складывем рандомные числа
+    }
+  }
+  return result;
+}
+
+void PrintArray(int[,] inArray)// функция вывода 2-мерного массива
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]} ");// сначала записываем все элементы перврго массива, потом все элементы второго массива и т.д
+    }
+    WriteLine();//переносим на новую строчку
+  }
+}
+
+void OrderArrayLines(int[,] array)// функция упорядочивает строки массива, а вход принимает 2-мерный массив
+{
+  for (int i = 0; i < array.GetLength(0); i++)// запускаем цикл который будет перебирать массивы(строки)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)// цикл перебирает (столбцы) 
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)// циклом перебираем элементы
+      {
+        if (array[i, k] < array[i, k + 1])// проверяем если в строке(i) элемент(k) меньше чем следующий то
+        {
+          int temp = array[i, k + 1];// записываем больший элемент
+          array[i, k + 1] = array[i, k];// меняем местами больший с меньшим
+          array[i, k] = temp;// результат записываем в переменную
+        }
+      }
+    }
+  }
+}
---
+++ Task_58/Program.cs
@@ -0,0 +1,80 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число строк 1-й матрицы: ");
+int m = int.Parse(ReadLine()!);
+Write("Введите число столбцов 1-й матрицы = строк 2-й матрицы: ");
+int n = int.Parse(ReadLine()!);
+Write("Введите число столбцов 2-й матрицы: ");
+int p = int.Parse(ReadLine()!);
+WriteLine();
+
+int[,] MartrixA = new int[m, n];
+GetArray(MartrixA);
+WriteLine("Первая матрица:");
+PrintArray(MartrixA);
+WriteLine();
+
+int[,] MartrixB = new int[n, p];
+GetArray(MartrixB);
+WriteLine("Вторая матрица:");
+PrintArray(MartrixB);
+WriteLine();
+
+int[,] MatrixC = new int[m, p];
+
+MultiplyMatrix(MartrixA, MartrixB, MatrixC);
+WriteLine("Произведение первой и второй матриц:");
+PrintArray(MatrixC);
+
+
+// функция умножения 2-х матриц
+void MultiplyMatrix(int[,] MartrixA, int[,] MartrixB, int[,] MatrixC)
+{
+  for (int i = 0; i < MatrixC.GetLength(0); i++)
+  {
+    for (int j = 0; j < MatrixC.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < MartrixA.GetLength(1); k++)
+      {
+        sum += MartrixA[i, k] * MartrixB[k, j];
+      }
+      MatrixC[i, j] = sum;
+    }
+  }
+}
+
+// функция для заполнения 2-мерного массива рандомными элементами
+void GetArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);// обращаемся к каждому элементу массива и складывем рандомные числа
+    }
+  }
+}
+
+void PrintArray(int[,] inArray)// функция вывода 2-мерного массива
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]} ");// сначала записываем все элементы перврго массива, потом все элементы второго массива и т.д
+    }
+    WriteLine();//переносим на новую строчку
+  }
+}
